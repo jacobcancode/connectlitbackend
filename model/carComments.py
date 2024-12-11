6a33804e10e7fabb4e0fb76d12514ac25c048bc2
JacksonH24 <jacksonh0924@gmail.com>
2024-12-11 10:34:51 -0800

    Car Comments backend completed

--- model/carComments.py
+++ model/carComments.py
@@ -1,19 +1,101 @@
 from sqlite3 import IntegrityError
 from sqlalchemy import Text
 from __init__ import app, db
-from model.user import User
-from model.group import Group
 from datetime import datetime
+from model.user import User
 
-class CarComment(db.Model):
+class CarComments(db.Model):
 
     __tablename__ = "carcomments" 
-     id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     _uid = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    _postid = db.column(db.Integer, db.ForeignKey("carPosts.id"), nullable=False)
-    _content = db.column(db.String(255), nullable=False)
+    _post_id = db.Column(db.Integer, db.ForeignKey("carPosts.id"), nullable=False)
+    _content = db.Column(db.String(255), nullable=False)
+    _date_posted = db.Column(db.DateTime, nullable=False)
+
 
     def __init__(self, uid, postid, content):
         self._uid = uid 
-        self._postid = postid
+        self._post_id = postid
         self._content = content
+        self._date_posted = datetime.now()
+
+    def __repr__(self):
+        """
+        The __repr__ method is a special method used to represent the object in a string format.
+        Called by the repr(post) built-in function, where post is an instance of the Post class.
+        
+        Returns:
+            str: A text representation of how to create the object.
+        """
+        return f"CarComment(id={self.id}, uid={self._uid}, postid={self._postid}, content={self._content}, date_posted={self._date_posted})"
+        
+    def create(self):
+        """
+        The create method adds the object to the database and commits the transaction.
+        
+        Uses:
+            The db ORM methods to add and commit the transaction.
+        
+        Raises:
+            Exception: An error occurred when adding the object to the database.
+        """
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise error
+        
+    def read(self):
+        """
+        The read method retrieves the object data from the object's attributes and returns it as a dictionary.
+        
+        Uses:
+            The Group.query and User.query methods to retrieve the group and user objects.
+        
+        Returns:
+            dict: A dictionary containing the post data, including user and group names.
+        """
+        user = User.query.get(self._uid)
+        data = {
+            "id": self.id,
+            "content": self._content,
+            "uid": user.id if user else None,
+            "post_id": self._post_id,
+            "date_posted": self._date_posted
+        }
+        return data
+    
+    def update(self):
+        """
+        The update method commits the transaction to the database.
+        
+        Uses:
+            The db ORM method to commit the transaction.
+        
+        Raises:
+            Exception: An error occurred when updating the object in the database.
+        """
+        try:
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise error
+        
+    def delete(self):
+        """
+        The delete method removes the object from the database and commits the transaction.
+        
+        Uses:
+            The db ORM methods to delete and commit the transaction.
+        
+        Raises:
+            Exception: An error occurred when deleting the object from the database.
+        """    
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise error
