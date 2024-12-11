f74e24a66bba2d4455db79acd0e5e7404002b1ba
WyattZCSP <wyattrobertzimmer@gmail.com>
2024-12-11 10:34:27 -0800

    finish table, add def init

--- model/userCars.py
+++ model/userCars.py
@@ -18,5 +18,10 @@ class UserCars(db.model):
     _color = db.Column(db.String(255), nullable=True) 
     _vin = db.Column(db.String(255), nullable=True) 
     _horsepower = db.Column(db.String(255), nullable=True) 
-    _0_to_60 = db.Column(db.String(255), nullable=True) 
+    _zero_to_sixty = db.Column(db.String(255), nullable=True) 
     _top_speed = db.Column(db.String(255), nullable=True) 
+
+    def __init__(self, uid, make, model, year, trim, engine_type, color, vin, horsepower, zero_to_sixty, top_speed):
+        if make not in ['audi, apollo, bentley, bmw, bugatti, ferrari, honda, hyundai, jaguar, koenigsegg, lamborghini, lancia, mclaren, mercedes, pagani, porsche, toyota']:
+            return {"message": "Bad Make!!"}, 404
+
