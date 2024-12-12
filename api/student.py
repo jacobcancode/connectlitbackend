7221d9413898efcd3a375104a5d631d56eeacb93
wendaobao <wendaobao@gmail.com>
2024-12-12 09:01:29 -0800

    added thomas to student.py

--- api/student.py
+++ api/student.py
@@ -20,7 +20,7 @@ class StudentAPI:
             "Kush": {
                 "name": "Kush",
                 "age": 15,
-                "major": "Bioogist",     
+                "major": "Biologist",     
                 "university": "UCS"
             },
             "Trevor": {
@@ -34,6 +34,12 @@ class StudentAPI:
                 "age": 16,
                 "major": "Finance",
                 "university": "Utah State"
+            },
+             "Thomas": {
+                "name": "Thomas",
+                "age": 17,
+                "major": "Biology",
+                "university": "UCB"
             },
         }
         return students.get(name)
@@ -61,6 +67,11 @@ class StudentAPI:
             # Use the helper method to get Jeff's details
             kush_details = StudentAPI.get_student("Kush")
             return jsonify(kush_details)
+    class _Thomas(Resource):
+        def get(self):
+            # Use the helper method to get Jeff's details
+            thomas_details = StudentAPI.get_student("Thomas")
+            return jsonify(thomas_details)
 
     class _Bulk(Resource):
         def get(self):
@@ -69,13 +80,15 @@ class StudentAPI:
             trevor_details = StudentAPI.get_student("Trevor")
             wyatt_details = StudentAPI.get_student("Wyatt")
             kush_details = StudentAPI.get_student("Kush")
-            return jsonify({"students": [gavin_details, trevor_details, wyatt_details, kush_details]})
+            thomas_details = StudentAPI.get_student("Thomas")
+            return jsonify({"students": [gavin_details, trevor_details, wyatt_details, kush_details, thomas_details]})
 
     # Building REST API endpoints
     api.add_resource(_Gavin, '/student/gavin')
     api.add_resource(_Trevor, '/student/trevor')
     api.add_resource(_Wyatt, '/student/wyatt')
     api.add_resource(_Kush, '/student/kush')
+    api.add_resource(_Thomas, '/student/thomas')
     api.add_resource(_Bulk, '/students')
 
 # Instantiate the StudentAPI to register the endpoints
