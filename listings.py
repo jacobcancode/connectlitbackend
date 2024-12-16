988db43e21efa044830d09d5f9efa1e9e734d182
kush1434 <kushkshah3@gmail.com>
2024-12-16 09:15:03 -0800

    api changes

--- listings.py
+++ listings.py
@@ -7,7 +7,6 @@ def fetch_listings(api_key, limit=5):
     response.raise_for_status()
     data = response.json()
 
-    # Extract relevant details from the "records" key
     listings = data.get("records", [])
     car_details = []
 
@@ -17,6 +16,7 @@ def fetch_listings(api_key, limit=5):
             "price": listing.get("price", "N/A"),
             "type": listing.get("bodyType", "N/A"),
             "mileage": listing.get("mileage", "N/A"),
+            "picture":listing.get("primaryPhotoUrl","N/A")
         }
         car_details.append(car)
 
@@ -25,7 +25,7 @@ def fetch_listings(api_key, limit=5):
 if __name__ == "__main__":
     API_KEY = "ZrQEPSkKa3VzaGtzaGFoM0BnbWFpbC5jb20="
     #move to a different place
-    cars = fetch_listings(API_KEY, limit=5)
+    cars = fetch_listings(API_KEY, limit=10)
     if cars:
         for idx, car in enumerate(cars, start=1):
             print(f"Listing {idx}:")
@@ -33,6 +33,7 @@ if __name__ == "__main__":
             print(f"  Price: {car['price']}")
             print(f"  Type: {car['type']}")
             print(f"  Mileage: {car['mileage']}")
+            print(f"  Photo Link: {car['picture']}")
             print()
     else:
         print("No listings found.")
