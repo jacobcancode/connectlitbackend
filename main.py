39157bc7d4ce4d9714fa02884ef40848742b7e1f
wendaobao <wendaobao@gmail.com>
2025-01-07 11:32:35 -0800

    fixed carchat

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from api.group import group_api
 from api.section import section_api
 from api.nestPost import nestPost_api # Justin added this, custom format for his website
 from api.messages_api import messages_api # Adi added this, messages for his website
-from api.carChat import car_chat_api
+from api.carChat import car_chat_bp
 from api.carPost import carPost_api
 from api.student import student_api
 from api.vin import vin_api
@@ -54,7 +54,7 @@ app.register_blueprint(post_api)
 app.register_blueprint(channel_api)
 app.register_blueprint(group_api)
 app.register_blueprint(section_api)
-app.register_blueprint(car_chat_api)
+app.register_blueprint(car_chat_bp)
 # Added new files to create nestPosts, uses a different format than Mortensen and didn't want to touch his junk
 app.register_blueprint(nestPost_api)
 app.register_blueprint(nestImg_api)
