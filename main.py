dda5740789906b8ce020ae658ac3f489f0e8418a
Tvick22 <142449252+Tvick22@users.noreply.github.com>
2025-01-15 17:20:26 -0800

    backup carPosts

--- main.py
+++ main.py
@@ -268,6 +268,7 @@ def extract_data():
         data['groups'] = [group.read() for group in Group.query.all()]
         data['channels'] = [channel.read() for channel in Channel.query.all()]
         data['posts'] = [post.read() for post in Post.query.all()]
+        data['carPosts'] = [post.read() for post in CarPost.query.all()]
     return data
 
 # Save extracted data to JSON files
@@ -282,7 +283,7 @@ def save_data_to_json(data, directory='backup'):
 # Load data from JSON files
 def load_data_from_json(directory='backup'):
     data = {}
-    for table in ['users', 'sections', 'groups', 'channels', 'posts']:
+    for table in ['users', 'sections', 'groups', 'channels', 'posts', 'carPosts']:
         with open(os.path.join(directory, f'{table}.json'), 'r') as f:
             data[table] = json.load(f)
     return data
