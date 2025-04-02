b31f4af4c68f314e9ee9a82e8374efa4806922f6
jacobcancode <jake.zierolf@gmail.com>
2025-04-02 10:34:08 -0700

    sigh

--- main.py
+++ main.py
@@ -407,7 +407,7 @@ app.cli.add_command(custom_cli)
 # this runs the flask application on the development server
 if __name__ == "__main__":
     # change name for testing
-    app.run(debug=True, host="0.0.0.0", port="8887")
+    app.run(debug=True, host="0.0.0.0", port="8888")
 
 
 # @app.route('/api/mechanicsTips', methods=['GET'])
